75b7cedfb81b20a8b94c33a25976af693e054fb7
Rom213husai <cejk1240196@gn.iwasaki.ac.jp>
2025-06-10 19:12:08 +0900

    APIを使って映画データを取得できるようにした

--- movie-app/src/App.tsx
+++ movie-app/src/App.tsx
@@ -1,10 +1,46 @@
+import {useEffect,useState} from "react";
 import './App.css'
 
+
+
 function App() {
+  //jsのコードが書ける
+  
+  const fetchMovieList = async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?language=ja&page=1`,
+      {
+        headers: {
+          Authorization:`Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data.results)
+    setMovieList(data.results);
+  }
+
+  useEffect(() =>{
+    fetchMovieList();
+  },[]);
+
+  const [movieList,setMovieList] = useState([]);
+  const [keyword,setKeyword] = useState("");
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    //htmlを書ける
+    <>
+      <input type="text" onChange={(e) => setKeyword(e.target.value)} />
+      
+      {movieList
+      .filter((movie) => movie.original_title.includes(keyword))
+      .map((movie) =>(
+        <div key={movie.id}>
+          <h2>{movie.original_title}</h2>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <p>{movie.overview}</p>
+        </div>
+      ))}
+    </>
   )
 }
 
