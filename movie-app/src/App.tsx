965a777d08ce3d158abfcb36f779d0277b5728d5
Rom213husai <cejk1240196@gn.iwasaki.ac.jp>
2025-06-13 13:47:20 +0900

    keywordの値が変更されると映画を再取得するようにした

--- movie-app/src/App.tsx
+++ movie-app/src/App.tsx
@@ -2,30 +2,65 @@ import {useEffect,useState} from "react";
 import './App.css'
 
 
+type Movie = {
+  id: string;
+  original_title: string;
+  poster_path: string;
+  overview: string;
+}
+
+type MovieJson = {
+  adult: boolean;
+  backdrop_path:string | null;
+  genre_ids: number[];
+  id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
 
 function App() {
   //jsのコードが書ける
   
   const fetchMovieList = async () => {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/movie/popular?language=ja&page=1`,
-      {
-        headers: {
-          Authorization:`Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
-        },
-      }
-    );
+    const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+    let url = "";
+    if(keyword) {
+      url = `https://api.themoviedb.org/3/search/movie?query=${keyword}&inckude_adult=false&language=ja&page=1`;
+    } else {
+      url = `https://api.themoviedb.org/3/movie/popular?language=ja&page=1`;
+    }
+    const response = await fetch(url,{
+      headers: {
+        Authorization:`Bearer ${API_KEY}`,
+      },
+    });
     const data = await response.json();
-    console.log(data.results)
-    setMovieList(data.results);
-  }
+    
+    setMovieList(
+      data.results.map((movie: MovieJson) =>({
+        id: movie.id,
+        original_title: movie.original_title,
+        poster_path: movie.poster_path,
+        overview: movie.overview,
+      }))
+    );
+  };
+
+  const [movieList,setMovieList] = useState<Movie[]>([]);
+  const [keyword,setKeyword] = useState("");
 
   useEffect(() =>{
     fetchMovieList();
-  },[]);
+  },[keyword]);
 
-  const [movieList,setMovieList] = useState([]);
-  const [keyword,setKeyword] = useState("");
   return (
     //htmlを書ける
     <>
